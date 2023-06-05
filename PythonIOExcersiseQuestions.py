5cc4891238acbae8d03afe654fb2ee90b1291638
Adil Kampoo <adil.kampu111@gmail.com>
2023-06-05 16:39:19 +0530

    Create PythonIOExcersiseQuestions.py

---
+++ PythonIOExcersiseQuestions.py
@@ -0,0 +1,26 @@
+#Write a program to accept two numbers from the user and calculate multiplication
+#Display three string “Name”, “Is”, “Heuristic” as “Name**Is**Heuristic”
+#Convert Decimal number to octal using print() output formatting
+#Display float number with 2 decimal places using print()
+#Accept a list of 5 float numbers as an input from the user
+numbers = []
+
+# 5 is the list size
+# run loop 5 times
+for i in range(0, 5):
+    print("Enter number at location", i, ":")
+    # accept float number from user
+    item = float(input())
+    # add it to the list
+    numbers.append(item)
+
+print("User List:", numbers)
+
+#File Handling Question skip for now
+#Write all content of a given file into a new file by skipping line number 5
+#Accept any three string from one input() call
+#Format variables using a string.format() method.
+#totalMoney = 1000
+#quantity = 3
+#price = 450
+
